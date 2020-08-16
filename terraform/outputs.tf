182ea3796d41c1c6561d2e71ac8d567da689aac6
alasundkar <alasundkar.r@northeastern.edu>
2020-08-15 20:26:45 -0700

    added terraform script

---
+++ terraform/outputs.tf
@@ -0,0 +1,30 @@
+output "this_vpc_id" {
+  description = "The ID of the VPC"
+  value       = "${aws_vpc.selected.id}"
+}
+
+output "this_subnet_primary_id" {
+  description = "The ID of the primary subnet"
+  value       = "${aws_subnet.primary-subnet.id}"
+}
+
+output "this_subnet_secondary_id" {
+  description = "The ID of the secondary subnet"
+  value       = "${aws_subnet.secondary-subnet.id}"
+}
+
+output "this_subnet_third_id" {
+  description = "The ID of the third subnet"
+  value       = "${aws_subnet.third-subnet.id}"
+}
+
+output "user_data" {
+  description = "User data passed to EC2 instance"
+  value = "${aws_launch_configuration.asg_launch_config.user_data}"
+}
+
+output "dns_name" {
+  description = "DNS name for load balancer"
+  value = "${aws_lb.webapp-load-balancer.dns_name}"
+}
+
